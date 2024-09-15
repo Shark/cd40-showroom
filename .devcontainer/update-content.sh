8812d2c8fbc1c4dad470f2eb62e01193b51de094
Felix Seidel <felix@seidel.me>
2024-09-15 07:42:27 +0000

    feat: install stack by default

--- .devcontainer/update-content.sh
+++ .devcontainer/update-content.sh
@@ -8,4 +8,7 @@ cd stack
 pushd .
 npm ci
 npm run build
+kubectl -n cd-40-system cp dist/stack.wasm $(kubectl get pod -n cd-40-system -l control-plane=controller-manager -o jsonpath="{.items[0].metadata.name}"):/workspace/stack-library/stack.wasm
 popd
+
+kubectl apply -f sample-app.yaml
