a034f3fce9bf6266d1d19d0b40a2ca524b5875e9
Felix Seidel <felix@seidel.me>
2024-10-03 03:40:09 +0000

    fix: update-content.sh deploy

--- .devcontainer/update-content.sh
+++ .devcontainer/update-content.sh
@@ -8,12 +8,7 @@ cd stack
 pushd .
 npm ci
 npm run build
-cd40_manager_pod=$(kubectl get pod -n cd-40-system -l control-plane=controller-manager -o jsonpath="{.items[0].metadata.name}")
-if [ -n "$cd40_manager_pod" ]; then
-  kubectl -n cd-40-system cp dist/stack.wasm "$cd40_manager_pod":/workspace/stack-library/stack.wasm
-else
-  echo "cd-40 manager pod not found"
-fi
+npm run deploy -- stack.wasm
 popd
 
 kubectl apply -f sample-app.yaml
