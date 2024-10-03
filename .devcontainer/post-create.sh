3310b0ceaa2112915ed398b64b9acc47da36d9b2
Felix Seidel <felix@seidel.me>
2024-10-03 09:30:50 +0000

    fix: wait for docker in post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ELAPSED_TIME=0
+until docker info &> /dev/null || [ $ELAPSED_TIME -ge 90 ]; do
+  echo "Waiting for Docker to be available..."
+  sleep 5
+  ELAPSED_TIME="$((ELAPSED_TIME+5))"
+done
+
 cd40_engine_version=v0.0.1-develop.2
 
 # Credentials not available in on-create.sh for Codespace prebuild, so we need to pull the image here
