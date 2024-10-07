8fa14e549e978da7b73dfeb6407486b2192e32d7
Felix Seidel <felix@seidel.me>
2024-10-07 11:40:53 +0000

    fix: compile with e2e test

--- stack/bin/bundle.js
+++ stack/bin/bundle.js
@@ -2,7 +2,7 @@ import { build as esbuild } from "esbuild";
 import { polyfillNode } from "esbuild-plugin-polyfill-node";
 
 await esbuild({
-  entryPoints: ["dist/stack.js"],
+  entryPoints: ["dist/src/stack.js"],
   bundle: true,
   outfile: "dist/bundle.js",
   format: "esm",
--- stack/tests/e2e/e2e.test.ts
+++ stack/tests/e2e/e2e.test.ts
@@ -1,7 +1,17 @@
-import { stack } from '../../dist/stack-js/stack.js'
+class ImportError extends Error {}
+
+const loadModule = async (modulePath: string) => {
+  try {
+    return await import(modulePath)
+  } catch (e) {
+    throw new ImportError(`Unable to import module ${modulePath}`)
+  }
+}
 
 describe('end-to-end', () => {
   test('run render', async () => {
+    // dynamically load because stack-js doesn't always exist
+    const { stack } = await loadModule('../../dist/stack-js/stack.js')
     const actual = stack.render({
       this: {
         metadata: {
